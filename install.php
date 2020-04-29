662871a69a7cf626b8aaaf5f29c943a938a3669f
reinhardp <rpirpag@gmx.at>
2020-04-29 07:32:38 +0200

    some fixes and enhancements

--- install.php
+++ install.php
@@ -9,7 +9,7 @@ class Install {
     /******/
 
     private $env = '.env';
-    private $zipFile = 'iventorycontrol-master.zip';
+    private $zipFile = 'inventorycontrol-master.zip';
     private $isWin = false;
     private $update = false;
 
@@ -54,44 +54,47 @@ class Install {
 
     private function extract() {
          
-        $retCode = exec("unzip $zipFile", $retArr, $retVal);
-        echo "extract: $retCode\n";
+        $retCode = exec("unzip $this->zipFile", $retArr, $retVal);
+        echo "extract: $retCode\n\n";
         
     }
 
     private function renameDir() {
-        $retCode = rename('iventorycontrol-master', 'iventorycontrol');
-        echo "renameDir: $retCode\n";
+        $retCode = rename('inventorycontrol-master', 'inventorycontrol');
+        echo "renameDir: $retCode\n\n";
     }
     
     private function copyDir() {
-        
-        $retCode = copy('iventorycontrol', $this->wwwRoot);
-        echo "copyDir: $retCode\n";
+        $dest = $this->wwwRoot . '/inventorycontrol';
+        $retCode = exec("mv inventorycontrol $dest");
+        //$retCode = rename('./inventorycontrol', $this->wwwRoot . "/inventorycontrol");
+        echo "copyDir: $retCode\n\n";
     }
     
     private function copyEnvFile() {
-        $retCode = copy('.env', $this->wwwRoot . '/inventorycontrol/.env');
-        echo "copyEnvFile: $retCode\n";
+        $retCode = copy($this->env, $this->wwwRoot . "/inventorycontrol/$this->env");
+        echo "copyEnvFile: $retCode\n\n";
     }
     
     
     private function generteKey($icrootpath) {
-        $command = "php " . $icrootpath . "/artisan key:generate";
+        $command = "php " . $icrootpath . "artisan key:generate";
+        //echo "The command: " . $command . "\n";
         $retCode = exec($command, $retArr, $retVal);        
-        echo "generteKey: $retCode\n";
+        echo "generteKey: $retCode\n\n";
     }
 
     private function migrate($icrootpath) {
-        $command = "php " . $icrootpath . "/artisan migrate";
+        $command = "php " . $icrootpath . "artisan migrate";
+        echo "Type yes to proceed the migration\n";
         $retCode = exec($command, $retArr, $retVal);        
-        echo "migrate: $retCode\n";
+        echo "migrate: $retCode\n\n";
         
     }
     
     private function message() {
-        echo "Add the con job for couter reset\n";
-        echo "The command have to be called from $this->wwwRoot/inventorycontrol/ directory"; 
+        echo "Add the cron job for couter reset.\n";
+        echo "The command have to be called from $this->wwwRoot/inventorycontrol directory.\n"; 
         echo "weekly: php artisan weeklyreset:cron\n";
         echo "monthly: php artisan monthlyreset:cron\n";
         echo "yearly: php artisan yearlyreset:cron\n";
